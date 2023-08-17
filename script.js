e09b817b7affae0eaece8380d9d94e0b1e1c16cf
navneet-chugh <navneetchugh15@gmail.com>
2023-08-17 20:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+  const rad = document.getElementById("radius").value;
+  const vol = document.getElementById("volume");
+  vol.value = ((4 / 3) * 3.1415928 * rad * rad * rad).toFixed(4);
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
